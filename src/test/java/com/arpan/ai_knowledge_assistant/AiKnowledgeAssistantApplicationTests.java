7fcce8e000c78e1dec48c68ea0725cc094c15bff
arpanmalik <arpanmalik2000@gmail.com>
2026-07-02 22:42:38 +0530

    lets start the best project

---
+++ src/test/java/com/arpan/ai_knowledge_assistant/AiKnowledgeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpan.ai_knowledge_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiKnowledgeAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
